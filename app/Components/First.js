b92b3c832a91e1128fc85acf0a67e7b6d66af228
isidro <idperez@asu.edu>
2017-01-28 20:00:24 -0700

    tab bar

---
+++ app/Components/First.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import {
+    View,
+    Text
+} from 'react-native';
+
+
+
+export default class SplashContainer extends Component {
+
+    render() {
+        return (
+            <View>
+                <Text>First</Text>
+            </View>
+        );
+    }
+}
---
+++ index.ios.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    TabBarIOS,
+    StyleSheet
+} from 'react-native';
+
+import { Tabs, Tab, Icon } from 'react-native-elements'
+
+import First from './app/Components/First';
+
+export default class Western extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            selectedTab: 'home',
+        }
+    }
+
+    changeTab (selectedTab) {
+        this.setState({selectedTab})
+    }
+
+    render() {
+
+        const { selectedTab } = this.state;
+
+        return (
+            <Tabs>
+                <Tab
+                    titleStyle={styles.title}
+                    selectedTitleStyle={styles.selectedTitle}
+                    selected={selectedTab === 'home'}
+                    title={selectedTab === 'home' ? 'HOME' : null}
+                    renderIcon={() => <Icon style={styles.iconContainer} color={'#5e6977'} name='home' size={33} />}
+                    renderSelectedIcon={() => <Icon color={'#6296f9'} name='home' size={30} />}
+                    onPress={() => this.changeTab('home')}>
+                    <First/>
+                </Tab>
+                <Tab
+                    titleStyle={styles.title}
+                    selectedTitleStyle={styles.selectedTitle}
+                    selected={selectedTab === 'yardage'}
+                    title={selectedTab === 'yardage' ? 'YARDAGE' : null}
+                    renderIcon={() => <Icon style={styles.iconContainer} color={'#5e6977'} name='golf-course' size={33} />}
+                    renderSelectedIcon={() => <Icon color={'#6296f9'} name='golf-course' size={30} />}
+                    onPress={() => this.changeTab('yardage')}>
+                    <First/>
+                </Tab>
+                <Tab
+                    titleStyle={styles.title}
+                    selectedTitleStyle={styles.selectedTitle}
+                    selected={selectedTab === 'eat'}
+                    title={selectedTab === 'eat' ? 'EAT' : null}
+                    renderIcon={() => <Icon style={styles.iconContainer} color={'#5e6977'} name='restaurant-menu' size={33} />}
+                    renderSelectedIcon={() => <Icon color={'#6296f9'} name='restaurant-menu' size={30} />}
+                    onPress={() => this.changeTab('eat')}>
+                    <First/>
+                </Tab>
+                <Tab
+                    titleStyle={styles.title}
+                    selectedTitleStyle={styles.selectedTitle}
+                    selected={selectedTab === 'more'}
+                    title={selectedTab === 'more' ? 'MORE' : null}
+                    renderIcon={() => <Icon style={styles.iconContainer} color={'#5e6977'} name='list' size={33} />}
+                    renderSelectedIcon={() => <Icon color={'#6296f9'} name='list' size={30} />}
+                    onPress={() => this.changeTab('more')}>
+                    <First/>
+                </Tab>
+            </Tabs>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    title: {
+        fontWeight: 'bold',
+        fontSize: 10
+    },
+    selectedTitle: {
+        marginTop: -1,
+        marginBottom: 6
+    },
+    iconContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 12
+    }
+});
+
+AppRegistry.registerComponent('Western', () => Western);
