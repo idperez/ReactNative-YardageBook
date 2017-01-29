1c6ffb210bf81cf6bcf6863825dc066da292d234
isidro <idperez@asu.edu>
2017-01-28 22:20:36 -0700

    header for yardage activity

--- app/Headers/YardageHeader.js
+++ app/Headers/YardageHeader.js
@@ -9,14 +9,20 @@ import {
 
 import NavigationBar from 'react-native-navbar';
 
-export default class Header extends Component {
+import { Icon } from 'react-native-elements'
+
+export default class YardageHeader extends Component {
 
     render() {
         return (
             <View style={{flex: 1, backgroundColor: '#2c3e50',}}>
                 <NavigationBar
                     title={<Image source={require('./../Images/logo.png')}
-                            style={{width: 60, height: 40}} /> }
+                                  style={{width: 60, height: 40}} /> }
+                    leftButton={<Icon
+                        name='chevron-left' />}
+                    rightButton={<Icon
+                        name='chevron-right' />}
                 />
             </View>
         );
