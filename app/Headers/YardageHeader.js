ffdfa2328ad081af28591a88412c5e5e1fef5a9b
isidro <idperez@asu.edu>
2017-01-29 21:47:02 -0700

    yardage container

--- app/Headers/YardageHeader.js
+++ app/Headers/YardageHeader.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 
 import {
     Navigator,
-    Image
+    Image,
+    View,
+    Text,
+    StyleSheet,
+    ScrollView
 } from 'react-native';
 
 import NavigationBar from 'react-native-navbar';
@@ -13,18 +17,47 @@ export default class YardageHeader extends Component {
 
     render() {
         return (
-            <NavigationBar
-                title={<Image
-                    source={require('./../Images/logo.png')}
-                    style={{width: 60, height: 40}}
-                    /> }
-                leftButton={<Icon
-                    name='chevron-left'
-                    /> }
-                rightButton={<Icon
-                    name='chevron-right'
-                    /> }
-            />
+            <ScrollView keyboardShouldPersistTaps="always" style={styles.container}>
+                <View>
+                    <NavigationBar
+                        title={<Image
+                            source={require('./../Images/logo.png')}
+                            style={{width: 60, height: 40}}
+                            /> }
+                        leftButton={<Icon
+                            name='chevron-left'
+                            /> }
+                        rightButton={<Icon
+                            name='chevron-right'
+                            /> }
+                    />
+                </View>
+            </ScrollView>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#2c3e50'
+    },
+    image: {
+        width: 312,
+        height: 180,
+        justifyContent: 'center',
+        alignItems: 'center',
+        resizeMode: 'contain'
+    },
+    cardText: {
+        marginBottom: 10,
+        marginTop: 10,
+        textAlign: 'center'
+    },
+    teeButton: {
+        borderRadius: 0,
+        marginLeft: 0,
+        marginRight: 0,
+        marginBottom: 0
+    }
+});
--- app/Yardage/YardageContainer.js
+++ app/Yardage/YardageContainer.js
@@ -2,20 +2,132 @@ import React, { Component } from 'react';
 
 import {
     View,
-    Text,
-    StyleSheet
+    StyleSheet,
+    ScrollView
 } from 'react-native';
 
-import Header from './../Headers/YardageHeader';
+import YardageHeader from './../Headers/YardageHeader';
+
+import { Text, Card, ListItem, Grid, Col, Row } from 'react-native-elements'
 
 export default class YardageContainer extends Component {
 
     render() {
         return (
-            <View style={styles.container}>
-                <Header/>
-                <Text>Yardage Page.</Text>
-            </View>
+            <ScrollView keyboardShouldPersistTaps="always" style={styles.container}>
+                <View>
+                    <YardageHeader/>
+                    <Grid>
+                        <Col>
+                            <Card
+                                title='Hole'>
+                                <Text style={styles.holeData} h3 >
+                                    1
+                                </Text>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card
+                                title='Par'>
+                                <Text style={styles.holeData} h3 >
+                                    3
+                                </Text>
+                            </Card>
+                        </Col>
+                    </Grid>
+                    <Card
+                        title="Yardage From Hole">
+                        <Grid>
+                            <Col>
+                                <Text style={styles.yardageLocation} >
+                                    Front
+                                </Text>
+                            </Col>
+                            <Col>
+                                <Text style={styles.yardageLocation} >
+                                    Middle
+                                </Text>
+                            </Col>
+                            <Col>
+                                <Text style={styles.yardageLocation} >
+                                    Front
+                                </Text>
+                            </Col>
+                        </Grid>
+                        <Grid>
+                            <Col>
+                                <Text style={styles.holeData} h3 >
+                                    324
+                                </Text>
+                            </Col>
+                            <Col>
+                                <Text style={styles.holeData} h3 >
+                                    234
+                                </Text>
+                            </Col>
+                            <Col>
+                                <Text style={styles.holeData} h3 >
+                                    342
+                                </Text>
+                            </Col>
+                        </Grid>
+                    </Card>
+                    <Card
+                        title="Tee Distance">
+                        <Grid>
+                            <Col>
+                                <Text style={styles.championship} h3 >
+                                    375
+                                </Text>
+                            </Col>
+                            <Col>
+                                <Text style={styles.middle} h3 >
+                                    365
+                                </Text>
+                            </Col>
+                        </Grid>
+                        <Grid>
+                            <Col>
+                                <Text style={styles.forward} h3 >
+                                    300
+                                </Text>
+                            </Col>
+                            <Col>
+                                <Text style={styles.junior} h3 >
+                                    290
+                                </Text>
+                            </Col>
+                        </Grid>
+                    </Card>
+                    <Card
+                        title="Handicap">
+                        <Grid>
+                            <Col>
+                                <Text style={styles.yardageLocation} >
+                                    Men
+                                </Text>
+                            </Col>
+                            <Col>
+                                <Text style={styles.yardageLocation} >
+                                    Women
+                                </Text>
+                            </Col>
+                        </Grid>
+                        <Grid>
+                            <Col>
+                                <Text style={styles.holeData} h3 >
+                                    2
+                                </Text>
+                            </Col>
+                            <Col>
+                                <Text style={styles.holeData} h3 >
+                                    3
+                                </Text>
+                            </Col>
+                        </Grid>
+                    </Card>
+                </View>
+            </ScrollView>
         );
     }
 }
@@ -24,6 +136,57 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#2c3e50'
+    },
+    cardText: {
+        textAlign: 'center'
+    },
+    teeText: {
+        textAlign: 'center',
+        marginBottom: 20,
+        fontWeight: 'bold',
+        fontStyle: 'italic'
+    },
+    yardageLocation: {
+        textAlign: 'center',
+        marginBottom: 20,
+    },
+    holeData: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontStyle: 'italic'
+    },
+    championship: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontStyle: 'italic',
+        marginBottom: 20,
+        color: '#2c3e50'
+    },
+    middle: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontStyle: 'italic',
+        marginBottom: 20,
+        color: '#FFC107'
+    },
+    forward: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontStyle: 'italic',
+        marginBottom: 20,
+        color: '#FF5722'
+    },
+    junior: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontStyle: 'italic',
+        marginBottom: 20,
+        color: '#00BCD4'
+    },
+    circle: {
+        width: 40,
+        height: 40,
+        borderRadius: 100/2,
+        backgroundColor: 'red'
     }
 });
-
