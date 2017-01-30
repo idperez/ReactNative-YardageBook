db93db5b44c30ae0817a53368e45daee89e3cd2b
isidro <idperez@asu.edu>
2017-01-29 20:45:18 -0700

    eat container

--- app/Eat/EatContainer.js
+++ app/Eat/EatContainer.js
@@ -1,21 +1,100 @@
 import React, { Component } from 'react';
-
 import {
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    Image,
+    Linking,
+    ScrollView,
+    List,
+    ListItem
 } from 'react-native';
 
 import Header from './../Headers/Header';
+import FitImage from 'react-native-fit-image'
+import Communications from 'react-native-communications';
+
+import { SocialIcon, Card, Button } from 'react-native-elements'
 
 export default class EatContainer extends Component {
 
     render() {
+        const facebook = 'https://www.facebook.com/mulligansatwesternskies/';
+        const instagram = 'https://www.instagram.com/explore/locations/528238619/';
         return (
-            <View style={styles.container}>
-                <Header/>
-                <Text>Eat Page.</Text>
-            </View>
+            <ScrollView keyboardShouldPersistTaps="always" style={styles.container}>
+                <View>
+                    <Header/>
+                    <Card
+                        title='Our Restaurant'>
+                        <FitImage
+                            source={require('./../Images/eat.jpg')}
+                            style={styles.image}
+                        />
+                        <Text style={styles.cardText} >
+                            Good food, cold beverages & friendly faces is expected when visiting Mulligans
+                        </Text>
+                        <Button
+                            backgroundColor='#2ecc71'
+                            buttonStyle={styles.teeButton}
+                            iconRight
+                            icon={{name: 'phone'}}
+                            title='Call Mulligans'
+                            onPress={() => Communications.phonecall('4805458542', true)}/>
+                    </Card>
+                    <Card
+                        title='Hours' style={{marginBottom: 20}}>
+                        <Text style={styles.cardTextDay} >Monday</Text>
+                        <Text style={styles.cardText} >7:00AM - 8:00PM</Text>
+                        <Text style={styles.cardTextDay} >Tuesday</Text>
+                        <Text style={styles.cardText} >7:00AM - 8:00PM</Text>
+                        <Text style={styles.cardTextDay} >Wednesday</Text>
+                        <Text style={styles.cardText} >7:00AM - 8:00PM</Text>
+                        <Text style={styles.cardTextDay} >Thursday</Text>
+                        <Text style={styles.cardText} >7:00AM - 8:00PM</Text>
+                        <Text style={styles.cardTextDay} >Friday</Text>
+                        <Text style={styles.cardText} >7:00AM - 8:00PM</Text>
+                        <Text style={styles.cardTextDay} >Saturday</Text>
+                        <Text style={styles.cardText} >6:00AM - 7:00PM</Text>
+                        <Text style={styles.cardTextDay} >Sunday</Text>
+                        <Text style={styles.cardText} >6:00AM - 7:00PM</Text>
+
+                    </Card>
+                    <Card
+                        title='Specials' style={{marginBottom: 20}}>
+                        <Text style={styles.cardTextDay} >Monday</Text>
+                        <Text style={styles.cardText} >One pound of wings and a two domestic drafts for $10</Text>
+                        <Text style={styles.cardTextDay} >Tuesday</Text>
+                        <Text style={styles.cardText} >Taco Tuesday, $2 tacos, $3 Dos Equis, Coronas or Pacificos</Text>
+                        <Text style={styles.cardTextDay} >Wednesday</Text>
+                        <Text style={styles.cardText} >Kids eat FREE with paid adult.
+                            All you can eat Pasta Bar 5:00pm-7:00pm $12.95</Text>
+                        <Text style={styles.cardTextDay} >Thursday</Text>
+                        <Text style={styles.cardText} >Prime Rib Dinner $16.95</Text>
+                        <Text style={styles.cardTextDay} >Friday</Text>
+                        <Text style={styles.cardText} >ALL YOU CAN EAT Fish Fry</Text>
+                        <Text style={styles.cardTextDay} >Saturday</Text>
+                        <Text style={styles.cardText} >$3 well drinks, $3 domestic bottles</Text>
+                        <Text style={styles.cardTextDay} >Sunday</Text>
+                        <Text style={styles.cardText} >HAPPY HOUR ALL DAY</Text>
+
+                    </Card>
+                    <Card
+                        title='Follow Mulligans' style={{marginBottom: 20}}>
+                        <SocialIcon
+                            title='Facebook'
+                            button
+                            type='facebook'
+                        />
+                        <SocialIcon
+                            title='Instagram'
+                            button
+                            light
+                            type='instagram'
+                        />
+                    </Card>
+                </View>
+            </ScrollView>
         );
     }
 }
@@ -24,6 +103,28 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#2c3e50'
+    },
+    image: {
+        width: 300,
+        height: 180,
+        justifyContent: 'center',
+        alignItems: 'center',
+        resizeMode: 'contain'
+    },
+    cardText: {
+        marginBottom: 10,
+        textAlign: 'center',
+    },
+    cardTextDay: {
+        marginBottom: 10,
+        textAlign: 'center',
+        color: '#2ecc71'
+    },
+    teeButton: {
+        borderRadius: 0,
+        marginLeft: 0,
+        marginRight: 0,
+        marginBottom: 0
     }
 });
 
--- app/Home/HomeContainer.js
+++ app/Home/HomeContainer.js
@@ -3,19 +3,65 @@ import React, { Component } from 'react';
 import {
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    Image,
+    Linking,
+    ScrollView
 } from 'react-native';
 
 import Header from './../Headers/Header';
 
+import { SocialIcon, Card, ListItem, Button } from 'react-native-elements'
+
+
+
 export default class HomeContainer extends Component {
 
     render() {
+        const url = 'http://www.westernskiesgolf.com/request_tt/';
+        const facebook = 'https://www.facebook.com/Western-Skies-Golf-Club-128509396749/';
+        const instagram = 'https://www.instagram.com/westernskiesgolfclub/';
+        const twitter = 'https://twitter.com/westernskiesgc';
         return (
-            <View style={styles.container}>
+            <ScrollView keyboardShouldPersistTaps="always" style={styles.container}>
+            <View>
                 <Header/>
-                <Text>Home Page.</Text>
+                <Card
+                    title='Welcome'>
+                    <Image
+                        source={require('./../Images/home.jpg')}
+                        style={styles.image}
+                    />
+                    <Text style={styles.cardText} >
+                        If you don't have a tee time, go make one already! Stop by Mulligans after your round for food and drinks.
+                    </Text>
+                    <Button
+                        backgroundColor='#2ecc71'
+                        buttonStyle={styles.teeButton}
+                        title='Book Tee Time'
+                        onPress={() => Linking.openURL(url)}/>
+                </Card>
+                <Card
+                    title='Follow Us' style={{marginBottom: 20}}>
+                    <SocialIcon
+                        title='Facebook'
+                        button
+                        type='facebook'
+                    />
+                    <SocialIcon
+                        title='Instagram'
+                        button
+                        light
+                        type='instagram'
+                    />
+                    <SocialIcon
+                        title='Twitter'
+                        button
+                        type='twitter'
+                    />
+                </Card>
             </View>
+            </ScrollView>
         );
     }
 }
@@ -24,6 +70,24 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#2c3e50'
+    },
+    image: {
+        width: 312,
+        height: 180,
+        justifyContent: 'center',
+        alignItems: 'center',
+        resizeMode: 'contain'
+    },
+    cardText: {
+        marginBottom: 10,
+        marginTop: 10,
+        textAlign: 'center'
+    },
+    teeButton: {
+        borderRadius: 0,
+        marginLeft: 0,
+        marginRight: 0,
+        marginBottom: 0
     }
 });
 
